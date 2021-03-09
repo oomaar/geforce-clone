aa39d030461d1635af6254edd9d997027043235f
Omar <omaar_5@hotmail.com>
2021-03-09 18:11:34 +0200

    complete carousel

--- Components/SliderImage/SliderImage.js
+++ Components/SliderImage/SliderImage.js
@@ -4,6 +4,10 @@ import {
     Slide,
     SlideImage,
     PSLayer1,
+    Title,
+    SubTitle,
+    CarouselBtn,
+    CarouselFeature,
 } from "./styledSliderImage";
 
 const SliderImage = ({ activeIndex, imgsData }) => {
@@ -14,6 +18,11 @@ const SliderImage = ({ activeIndex, imgsData }) => {
                 {imgsData.map(image => (
                     <Slide key={image.id} className={image.id === activeIndex ? "active" : "inactive"}>
                         <SlideImage src={image.url} alt={image.alt} />
+                        <CarouselFeature>
+                            <Title loon={image.titleColor}>{image.title}</Title>
+                            <SubTitle loon={image.subTitleColor}>{image.subTitle}</SubTitle>
+                            <CarouselBtn>{image.buttonText}</CarouselBtn>
+                        </CarouselFeature>
                     </Slide>
                 ))}
             </SubContainer>
--- Components/SliderImage/styledSliderImage.js
+++ Components/SliderImage/styledSliderImage.js
@@ -5,6 +5,11 @@ export const Container = styled.div`
     height: 100%;
     margin-left: auto;
     z-index: 0;
+
+    @media screen and (max-width: 1000px) {
+        width: 100%;
+        height: 70%;
+    }
 `;
 
 export const SubContainer = styled.div`
@@ -27,6 +32,10 @@ export const SlideImage = styled.img`
     width: 100%;
     height: 100%;
     object-fit: contain; 
+
+    @media screen and (max-width: 1000px) {
+        margin: 100px 0 0;
+    }
 `;
 
 export const PSLayer1 = styled.div`
@@ -39,6 +48,54 @@ export const PSLayer1 = styled.div`
     background: 
     linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(100, 100, 100, 0.22), rgba(0, 0, 0, 0.36));
     z-index: 10;
-    /* border: 4px solid red; */
-    /* display: none; */
+`;
+
+export const CarouselFeature = styled.div`
+    position: absolute;
+    top: 35%;
+    left: 18%;
+    z-index: 200;
+
+    @media screen and (max-width: 1000px) {
+        top: 5%;
+        left: 5%;
+    }
+`;
+
+export const Title = styled.h1`
+    color: ${({loon}) => loon};
+    font-size: 60px;
+    font-size: clamp(2.092rem, 5vw, 4rem);
+    text-transform: uppercase;
+    font-family: 'Geforce', Helvetica, sans-serif;
+`;
+
+export const SubTitle = styled.h1`
+    color: ${({loon}) => loon};
+    font-family: 'Geforce', Helvetica, sans-serif;
+    max-width: 450px;
+    text-transform: uppercase;
+    font-size: clamp(1.125rem, 3vw, 1.625rem);
+`;
+
+export const CarouselBtn = styled.button`
+    border: none;
+    outline: none;
+    color: #fff;
+    text-transform: uppercase;
+    font-size: 1rem;
+    font-weight: bold;
+    background: ${({theme}) => theme.colors.primary};
+    padding: 10px;
+    transition: 0.5s ease;
+    cursor: pointer;
+    margin-top: 30px;
+
+    &:hover {
+        background: ${({theme}) => theme.colors.hover};
+    }
+
+    @media screen and (max-width: 1000px) {
+        margin-top: 10px;
+    }
 `;
